bffd9acaf59d6b5dbfcdf73d0cfccd19620cd097
Abdullah123513 <38668568+Abdullah123513@users.noreply.github.com>
2022-08-15 12:36:19 +0600

    Update main.c

--- main.c
+++ main.c
@@ -1,45 +1,34 @@
 #include <stdio.h>
 
-int main()
-{
-    int n;
-    printf("How many number you want to compire : ");
-    scanf("%d",&n);
-    if (n>1)
-    {
+int main() {
+  int n;
+  printf("How many number you want to compire : ");
+  scanf("%d", & n);
+  if (n > 1) {
     int sNumber[n];
-    printf("Enter %d number : ",n);
-    for(int i=0;i<n;i++)
-        {
-            scanf("%d",&sNumber[i]);
-        }
-    int min = sNumber[0],max=0,sum=0;
-    for(int k=0;k<n;k++)
-    {
-        if(max<sNumber[k])
-        {
-        max=sNumber[k];
-    
-            
-        }
-        if(min>sNumber[k])
-        {
-            min=sNumber[k];
-        }
-        
-        sum=sum+sNumber[k];
+    printf("Enter %d number : ", n);
+    for (int i = 0; i < n; i++) {
+      scanf("%d", & sNumber[i]);
     }
-    printf("max Number is = %d\n",max);
-    printf("min Number is = %d\n",min);
-    printf("Sum of all number = %d\n",sum);
-        
-        
-        
-        
-    }
-    else{
-        printf("Invalid Number!");
-    }
+    int min = sNumber[0], max = 0, sum = 0;
+    for (int k = 0; k < n; k++) {
+      if (max < sNumber[k]) {
+        max = sNumber[k];
 
-    return 0;
+      }
+      if (min > sNumber[k]) {
+        min = sNumber[k];
+      }
+
+      sum = sum + sNumber[k];
+    }
+    printf("max Number is = %d\n", max);
+    printf("min Number is = %d\n", min);
+    printf("Sum of all number = %d\n", sum);
+
+  } else {
+    printf("Invalid Number!");
+  }
+
+  return 0;
 }
